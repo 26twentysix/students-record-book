e3d79a5a8085c693792abf5a410bbaac6410173f
twentysix <54219970+26twentysix@users.noreply.github.com>
2020-04-06 03:03:52 +0500

    переделал группу в динамический двусвязный список + стилистические исправления

--- source.cpp
+++ source.cpp
@@ -1,35 +1,52 @@
 #include <iostream>
-#include <fstream>
 #include <string>
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
-class Group {	
+bool SortNames(string name1, string name2) {
+	vector<string> v = { name1, name2 };
+	for (size_t i = 0; i < 2; i++){
+		for (size_t j = i + 1; j < 2; j++) {
+			if (strcmp(v[i].c_str(), v[j].c_str()) > 0)
+			{
+				string swap = v[i];
+				v[i] = v[j];
+				v[j] = swap;
+			}
+		}
+	}
+	return (name1 == v[0]);
+}
+
+class Group {
 public:
-	class Student {
+	class Record {
 	public:
-		string student_name;
-		void set_name(string name) {
-			student_name = name;
+		void SetName(string name) {
+			studentName = name;
 		}
-		void add_exam(string exam_name, string exam_date, string exam_result){
-			exams.push_back(exam_name + '\t' + exam_date + '\t' + exam_result + '\n');
+		string GetName() {
+			return studentName;
 		}
-		void print_zachetka() {
-			cout << student_name + '\n';
+		void AddExam(string examName, string examDate, string examResult) {
+			exams.push_back(examName + '\t' + examDate + '\t' + examResult + '\n');
+		}
+		void PrintRecord() {
+			cout << studentName << endl;
 			for (size_t i = 0; i < exams.size(); i++) {
 				cout << exams[i];
 			}
 		}
-		void remove_exam(string name) {
+		void RemoveExam(string examName) {
 			for (size_t i = 0; i < exams.size(); i++) {
-				for (size_t j = 0; j < name.length() + 1; j++) {
-					if (j == name.length()) {
+				for (size_t j = 0; j < examName.length() + 1; j++) {
+					if (j == examName.length()) {
 						exams.erase(exams.begin() + i);
 						break;
 					}
-					if (exams[i][j] != name[j]) {
+					if (exams[i][j] != examName[j]) {
 						j = 0;
 						break;
 					}
@@ -37,68 +54,161 @@ public:
 			}
 		}
 	private:
+		string studentName;
 		vector<string> exams;
 	};
-	void set_group_name(string name) {
-		group_name = name;
+	struct Student {
+		Record studentRecord;
+		Student* prev;
+		Student* next;
+	};
+	void SetGroupName(string name) {
+		groupName = name;
 	}
-	void print_group_name() {
-		cout << group_name;
+	void AddStudent(string name) {
+		if (studentsCount == 0) {
+			head = new Student;
+			head->studentRecord.SetName(name);
+			head->prev = nullptr;
+			head->next = nullptr;
+			studentsCount++;
+		}
+		else {
+			auto tmp = head;
+			string name1 = tmp->studentRecord.GetName();
+			auto newStudent = new Student;
+			newStudent->studentRecord.SetName(name);
+			while (SortNames(name1, name) && tmp->next != nullptr) {
+				tmp = tmp->next;
+				name1 = tmp->studentRecord.GetName();
+			}
+			if (tmp->next == nullptr && SortNames(name1,name)) {
+				tmp->next = newStudent;
+				newStudent->prev = tmp;
+				newStudent->next = nullptr;
+			}
+			else if (!SortNames(name1, name) && tmp->next == nullptr) {
+				auto tmpNext = tmp;
+				head = newStudent;
+				head->prev = nullptr;
+				head->next = tmpNext;
+				tmpNext->prev = head;
+			}
+			else {
+				auto prevTmp = tmp->prev;
+				newStudent->next = tmp;
+				newStudent->prev = prevTmp;
+				prevTmp->next = newStudent;
+				tmp->prev = newStudent;
+			}
+			studentsCount++;
+		}
 	}
-	void add_student(string name) {
-		Student tmp;
-		tmp.set_name(name);
-		students.push_back(tmp);
-	}
-	void find_student(string name) {
-		for (size_t i = 0; i < students.size(); i++) {
-			if (students[i].student_name == name) {
-				cout << "Студент " << name << " имеет номер " << i + 1 << endl;
+	void FindStudent(string name) {
+		if (studentsCount == 0) {
+			cout << "Что бы найти студента, сначала нужно добавить студента" << endl;
+		}
+		else {
+			auto tmp = head;
+			size_t count = 0;
+			while (tmp->studentRecord.GetName() != name && count <= studentsCount) {
+				tmp = tmp->next;
+				count++;
+			}
+			if (count > studentsCount) {
+				cout << "Такого студента нет" << endl;
+			}
+			else {
+				cout << "Студент " << name << " в списке под номером " << (count + 1) << endl;
 			}
 		}
 	}
-	void add_student_exam(size_t num, string exam_name, string exam_date, string exam_result) {
-		students[num - 1].add_exam(exam_name, exam_date, exam_result);
+	void AddStudentExam(string name, string examName, string examDate, string examResult) {
+		auto tmp = head;
+		while (tmp->studentRecord.GetName() != name) {
+			tmp = tmp->next;
+		}
+		tmp->studentRecord.AddExam(examName, examDate, examResult);
 	}
-	void remove_student_exam(size_t num, string name) {
-		students[num - 1].remove_exam(name);
+	void RemoveStudentExam(string name, string examName) {
+		auto tmp = head;
+		while (tmp->studentRecord.GetName() != name) {
+			tmp = tmp->next;
+		}
+		tmp->studentRecord.RemoveExam(examName);
 	}
-	void remove_student(size_t num) {
-		students.erase(students.begin() + (num - 1));
-	}
-	void print_student_zachetka(size_t num) {
-		cout << num << "." << " ";
-		students[num - 1].print_zachetka();
-	}
-	void print_group_zachetka() {
-		for (size_t i = 0; i < students.size(); i++) {
-			cout << i + 1 << "." << " ";
-			students[i].print_zachetka();
+	void RemoveStudent(string name) {
+		auto tmp = head;
+		if (studentsCount == 1) {
+			delete head;
+			studentsCount = 0;
+			return;
+		}
+		if (head->studentRecord.GetName() == name) {
+			tmp = head->next;
+			delete head;
+			head = tmp;
+			studentsCount--;
+			return;
+		}
+		while (tmp->studentRecord.GetName() != name) {
+			tmp = tmp->next;
+		}
+		auto tmpPrev = tmp->prev;
+		if (tmp->next == nullptr) {
+			tmpPrev->next = nullptr;
+			delete tmp;
+			studentsCount--;
+		}
+		else {
+			auto tmpNext = tmp->next;
+			tmpPrev->next = tmpNext;
+			tmpNext->prev = tmpPrev;
+			delete tmp;
+			studentsCount--;
 		}
 	}
+	void PrintStudentRecord(string name) {
+		auto tmp = head;
+		while (tmp->studentRecord.GetName() != name) {
+			tmp = tmp->next;
+		}
+		tmp->studentRecord.PrintRecord();
+	}
+	void PrintGroupRecord() {
+		auto tmp = head;
+		while (tmp->next != nullptr) {
+			tmp->studentRecord.PrintRecord();
+			tmp = tmp->next;
+		}
+		tmp->studentRecord.PrintRecord();
+	}
 private:
-	string group_name;
-	vector<Student> students;
+	Student* head;
+	string groupName;
+	size_t studentsCount = 0;
 };
 
 int main() {
 	setlocale(0, "");
 	Group mh;
-	mh.set_group_name("МХ-101");
-	mh.add_student("Кутас Сергей Витальевич");
+	mh.SetGroupName("МХ-101");
+	mh.AddStudent("Кутас Сергей Витальевич");
 	cout << "Поиск студента" << endl;
-	mh.find_student("Кутас Сергей Витальевич");
-	mh.add_student_exam(1, "Математический анализ", "14.01.2020", "неуд.");
-	mh.add_student_exam(1, "Алгебра и геометрия", "21.01.2020", "хор.");
-	mh.add_student("Кондратьева Анастасия Григорьевна");
-	mh.find_student("Кондратьева Анастасия Григорьевна");
-	mh.add_student_exam(2, "Математический анализ", "14.01.2020", "неуд.");
-	mh.remove_student_exam(1, "Алгебра и геометрия");
+	mh.FindStudent("Кутас Сергей Витальевич");
+	mh.AddStudentExam("Кутас Сергей Витальевич", "Математический анализ", "14.01.2020", "удовл.");
+	mh.AddStudentExam("Кутас Сергей Витальевич","Алгебра и геометрия", "21.01.2020", "хор.");
+	cout << endl << endl << "Зачетка Сергея" << endl;
+	mh.PrintStudentRecord("Кутас Сергей Витальевич");
+	mh.AddStudent("Кондратьева Анастасия Григорьевна");
+	mh.FindStudent("Кондратьева Анастасия Григорьевна");
+	mh.AddStudentExam("Кондратьева Анастасия Григорьевна", "Математический анализ", "14.01.2020", "удовл.");
+	mh.RemoveStudentExam("Кутас Сергей Витальевич", "Алгебра и геометрия");
 	cout << endl << endl << "Зачетка Анастасии" << endl;
-	mh.print_student_zachetka(2);
-	cout << endl << endl << "Зачетка группы" << endl;
-	mh.print_group_zachetka();
-	mh.remove_student(1);
+	mh.PrintStudentRecord("Кондратьева Анастасия Григорьевна");
+	cout << endl << endl << "Зачетка группы после удаления экзамена у Сергея" << endl;
+	mh.PrintGroupRecord();
+	mh.RemoveStudent("Кутас Сергей Витальевич");
 	cout << endl << endl << "Зачетка группы после удаления Cергея" << endl;
-	mh.print_group_zachetka();
+	mh.PrintGroupRecord();
 }
