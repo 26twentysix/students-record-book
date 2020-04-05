f7bac775192a2bb906536113dfde504ad32f5289
twentysix <54219970+26twentysix@users.noreply.github.com>
2020-04-06 03:00:06 +0500

    класс вместо структуры

--- source.cpp
+++ source.cpp
@@ -1,42 +1,42 @@
 #include <iostream>
-#include <string>
+#include <fstream>
+#include <cstring>
+
 using namespace std;
 
-struct Node {
-	string predmet;
-	string prepodovatel;
-	string ocenka;
-	Node* next;
+class Zachetka {	
+  public:
+	  string surname;
+	  string firstname;
+	  string patronymic;
+	  string exam_name[3];
+	  string date[3];
+	  size_t assessment[3];
 };
-
-void print(Node *zachetka) {
-	cout << zachetka->predmet << " " << zachetka->prepodovatel << " " << zachetka->ocenka << endl;
-	if (zachetka->next != NULL) {
-		print(zachetka->next);
-	}
-	return;
-}
 
 int main() {
 	setlocale(0, "");
-	Node* zachetka1str, * zachetka2str,* zachetka3str;
-	zachetka1str = new Node;
-	zachetka2str = new Node;
-	zachetka3str = new Node;
-	zachetka1str->predmet = "Основы программирования";
-	zachetka1str->prepodovatel = "Осипов Сергей Иванович        ";
-	zachetka1str->ocenka = "4(хор.)";
-	zachetka1str->next = zachetka2str;
-	zachetka2str->predmet = "Математический анализ  ";
-	zachetka2str->prepodovatel = "Антонов Николай Юрьевич       ";
-	zachetka2str->ocenka = "4(хор.)";
-	zachetka2str->next = zachetka3str;
-	zachetka3str->predmet = "Алгебра и геометрия    ";
-	zachetka3str->prepodovatel = "Овсянников Александр Яковлевич";
-	zachetka3str->ocenka = "4(хор.)";
-	zachetka3str->next = NULL;
-	cout << "Зачетная книжка студента УрФУ Кутас Сергея" << endl;
-	cout << "1 семестр" << endl << "Предмет                 " << "Преподаватель                  " << "Оценка      " << endl;
-	print(zachetka1str);
-
+	Zachetka my_zachetka;
+	const char* zachetka = "zachetka.txt";
+	my_zachetka.surname = "Кутас"; my_zachetka.firstname = "Сергей"; my_zachetka.patronymic = "Витальевич";
+	my_zachetka.exam_name[0] = "Математический анализ";
+	my_zachetka.date[0] = "14.01.2020";
+	my_zachetka.assessment[0] = 2;
+	my_zachetka.exam_name[1] = "Алгебра";
+	my_zachetka.date[1] = "21.01.2020";
+	my_zachetka.assessment[1] = 4;
+	my_zachetka.exam_name[2] = "Основы программирования";
+	my_zachetka.date[2] = "25.01.2020";
+	my_zachetka.assessment[2] = 4;
+	ofstream file (zachetka, ios::binary);
+	file.write((char*)& my_zachetka, sizeof(my_zachetka));
+	file.close();
+	memset(&my_zachetka, 0, sizeof(Zachetka));
+	ifstream read_file(zachetka, ios::binary);
+	read_file.read((char*)& my_zachetka, sizeof(my_zachetka));
+	read_file.close();
+	cout << my_zachetka.surname << ' ' << my_zachetka.firstname << ' ' << my_zachetka.patronymic << '\n';
+	for (size_t i = 0; i < 3; i++) {
+		cout << my_zachetka.exam_name[i] << ' ' << my_zachetka.date[i] << ' ' << my_zachetka.assessment[i] << '\n';
+	}
 }
